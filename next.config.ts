d743e93e21425204fe99a536ea9457bbf47d993d
balbirkaur <balbirkaur82@gmail.com>
2025-08-31 16:53:44 +0530

    Nextjs Dashboard
    Started Next.js Dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   devIndicators: {
+    position: "top-right", // top-right, bottom-right, top-left, bottom-left
+  }
 };
 
 export default nextConfig;
